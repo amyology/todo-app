68ed1141d80b929582a06d79efb2a5e19304862c
Amy Chan <chamy09@gmail.com>
2016-06-25 01:56:31 -0500

    clear completed

--- app/assets/javascripts/todo_controller.js
+++ app/assets/javascripts/todo_controller.js
@@ -28,14 +28,45 @@
       }
     };
 
-    $scope.strike = function(task) {
-        var taskList = $scope.tasks;
-        task.complete = !task.complete;
-    }
+    $scope.completeTask = function(task) {
+      task.complete = !task.complete;
+    };
 
     $scope.deleteTask = function(index) {
       $scope.tasks.splice(index, 1);
     };
+
+    $scope.clearAll = function(index) {
+      $scope.tasks = [];
+    };
+
+    $scope.countTasks = function() {
+      var completedTasks = [];
+      angular.forEach($scope.tasks, function(task, index) {
+        if (task.complete === false) {
+          completedTasks.push(task);
+        }
+      });
+      if (completedTasks.length === 1) {
+        return "1 more thing left to do."
+      }
+      else if (completedTasks.length !== 0) {
+        return completedTasks.length + " things left to do.";
+      }
+      else {
+        return "Nothing to do at the moment.";
+      }
+    };
+
+    $scope.clearCompleted = function() {
+      var activeTasks = []
+      angular.forEach($scope.tasks, function(task, index) {
+        if (task.complete === false) {
+          activeTasks.push(task)
+        }
+      });
+      $scope.tasks = activeTasks;
+    }
 
     window.scope = $scope;
 
